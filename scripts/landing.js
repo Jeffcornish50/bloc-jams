4d7fb5ecf9a59ea662d83db2f93eca99a307352e
Jeff Cornish <jeffcornish50@gmail.com>
2017-09-07 11:45:12 -0400

    completed checkpoint-9-events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,11 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-          var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+
 
           var revealPoint = function(index) {
 
-            for(var i = 0; i < points.length; i++) {
+            for(var i = 0; i < pointsArray.length; i++) {
               points[i].style.opacity = 1;
               points[i].style.transform = "scaleX(1) translateY(0)";
               points[i].style.msTransform = "scaleX(1) translateY(0)";
@@ -16,3 +17,16 @@ var animatePoints = function() {
           revealPoint(0);
 
       };
+
+      window.onload = function() {
+        if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+        var sellingPoints = document.getElementsByClassName('selling-points')[0];
+        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+        window.addEventListener('scroll', function(event) {
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+          }
+     });
+   }
